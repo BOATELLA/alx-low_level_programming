9a3b70f6725e92516fda0ff914c7fd3c68edc8e8
BOATELLA <adjoaboatengnana@gmail.com>
2023-06-18 07:34:53 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints the alphabet.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success) 
  */
 
-	int main(void)
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+       int i;
 
-{
-	int lowerCase = 'a';
-	int upperCase = 'A';
-	while  (lowerCase  <= 'z')
-{
-		putchar(lowerCase);
-		lowerCase += 1;
-}
-	while (upperCase <= 'z')
-{
-		putchar(upperCase);
-		upperCase += 1;
-}
-		putchar('\n');
+	for (i = 0; i < 26; i++)
+	{
+		putchar	(alp[i]);
+	}
+	putchar('\n');
 	return (0);
 }
